3ab14e292b1f0dbe22313d59460595ff766c24e2
Jucélio Brandão <juceneon@gmail.com>
2022-04-07 20:21:10 -0300

    impl(hello.c) Faz a leitura dos valores e de seus ponteiros

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, const char *argv[]) {
+  int x = 2;
+  int* p_x = &x;
   printf("Hello World!\n");
+  printf("Valor de x: %d\n", x);
+  printf("Endereço de memória de x: %p\n", &x);
+  printf("Valor de p_x: %p\n", p_x);
+  printf("Endereço de memória de p_x  %d\n", *p_x);
 }
